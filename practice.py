07d2c55d9ee1c2cb52a3501c4d24e75843807579
“[Benard <ngwabern@gmail.com>
2024-01-15 22:59:11 -0600

    Initial commit

---
+++ practice.py
@@ -0,0 +1,61 @@
+#Question 1
+
+# An intergers are python data types that numbers both positive and negative with no decimals
+# Floats are python data types that has decimals
+#Booleans are data types that have just two values True or False
+# String are alphabets be it numerical or letters and must be in quotes.
+
+#Question 2
+name= "Benard"
+print(name)      #outcome is Benard
+
+age= 50
+print(age)     #outcome is 50
+
+#Question 3
+#a)
+a=15
+b=7
+c=(a+b)
+print(c) #outcome 22
+
+#b)
+a=25
+b=9
+d=(a-b)
+print(d) #outcome 16
+
+#C)
+a=8
+b=6
+f=(a*b)
+print(f)  #outcome 48
+
+#d)
+a=36
+b=4
+u=(a/b)
+print(u)  #outcome is 9.0
+
+#e)
+a=2
+b=5
+r=(a**b)
+print(r) #outcome is 32
+
+#Question 4
+#a)
+total=50
+total +=20
+print(total)  #outcone is 70
+#b)
+count=25
+count -=5
+print(count)
+
+#Question 5
+print(10==10)
+print(15!=20)
+print(25>18)
+print(30<=30)
+print(40!="40")
